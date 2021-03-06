52b5c0a415f5bdd96282c14381fc49272ad82796
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-03-06 10:21:04 +0100

    js: add dims config to game

--- js/game.js
+++ js/game.js
@@ -1,3 +1,8 @@
-window.onload = () => {
-    var game = new Phaser.Game();
+
+var config = {
+    width: 900, // dims will depends on screen size later
+    height: 600,
+    backgroundColor: 0x000, 
 }
+
+var game = new Phaser.Game(config);
