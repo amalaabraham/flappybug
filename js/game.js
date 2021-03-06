d701a6362045a6937394dd272184243bc72fca72
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-03-06 10:46:41 +0100

    add template Menu Scence

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ var config = {
     width: 900, // dims will depends on screen size later
     height: 600,
     backgroundColor: 0x000, 
+    scene: [Menu, ],
 }
 
 var game = new Phaser.Game(config);
---
+++ js/gmenu.js
@@ -0,0 +1,12 @@
+class Menu extends Phaser.Scene {
+
+    constructor()
+    {
+        super('Menu');
+    }
+
+    create()
+    {
+        this.add.text(25, 25, "Game Menu Here", {fill: 'yellow'});
+    }
+}
