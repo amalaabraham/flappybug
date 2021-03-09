646aaab35d913bb25b2c658a79cdfb863414750b
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-03-09 10:06:00 +0100

    make gameWindow size fit screen - 80% of it

--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,20 @@
 
-var config = {
-    width: 900, // dims will depends on screen size later
-    height: 600,
-    backgroundColor: 0x000, 
-    scene: [Menu, ],
-}
+$( () => {
 
-var game = new Phaser.Game(config);
+    let gameHeight = Math.floor( $(document).height() * .8 );
+    let gameWidth = Math.floor( $(document).width() * .8 );
+
+    console.log(`game width = ${gameWidth}`);
+    console.log(`game height = ${gameHeight}`);
+
+
+    var config = {
+        width: gameWidth,
+        height: gameHeight,
+        backgroundColor: 0x000, 
+        scene: [Menu, ],
+    }
+    
+    var game = new Phaser.Game(config);
+
+});
