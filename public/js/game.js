bd9d504163f8b5e7935ea3ca5c163d246e3f8ab6
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2021-03-09 17:30:15 +0300

    add flappy bug assets

--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,8 @@
-
 var config = {
-    width: 900, // dims will depends on screen size later
-    height: 600,
-    backgroundColor: 0x000, 
-    scene: [Menu, ],
-}
+  width: 900, // dims will depends on screen size later
+  height: 600,
+  backgroundColor: 0x000,
+  scene: [Menu],
+};
 
 var game = new Phaser.Game(config);
