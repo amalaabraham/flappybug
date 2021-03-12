994314e62b740b941fd2328c74a599ccf8ef9198
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-03-12 21:23:16 +0100

    add ressources and implement game loading

--- public/js/game.js
+++ public/js/game.js
@@ -1,14 +1,14 @@
 var doDebug = false;
 
-$( () => {
-    
-    let DEBUG = (err) => {
-        if(doDebug)
-            console.log(err);
-    };
+var DEBUG = (err) => {
+    if (doDebug)
+        console.log(err);
+};
 
-    let gameHeight = Math.floor( $(document).height() * .8 );
-    let gameWidth = Math.floor( $(document).width() * .8 );
+$(() => {
+
+    let gameHeight = Math.floor($(document).height() * .8);
+    let gameWidth = Math.floor($(document).width() * .8);
 
     DEBUG(`game width = ${gameWidth}`);
     DEBUG(`game height = ${gameHeight}`);
@@ -17,10 +17,17 @@ $( () => {
     var config = {
         width: gameWidth,
         height: gameHeight,
-        backgroundColor: 0x000, 
-        scene: [Menu, ],
+        backgroundColor: 0x000,
+        physics:
+        {
+            default: "arcade",
+            arcade: {
+                gravity: { y: 0 }
+            }
+        },
+        scene: [Menu,],
     }
-    
-    var game = new Phaser.Game(config);
+
+    game = new Phaser.Game(config);
 
 });
--- public/js/gmenu.js
+++ public/js/gmenu.js
@@ -1,12 +1,75 @@
 class Menu extends Phaser.Scene {
 
-    constructor()
-    {
+
+    constructor() {
         super('Menu');
+
+        this.tilesets = null;
     }
 
-    create()
-    {
-        this.add.text(25, 25, "Game Menu Here", {fill: 'yellow'});
+    getObjPropertyFromGid(gid, prop) {
+        if (this.tilesets == null || this.tilesets == undefined) {
+            DEBUG("Object JSON Tilesets")
+            return null;
+        }
+
+        for (let i = 0; i < this.tilesets.length; i++) {
+            let obj = this.tilesets[i]
+            if (obj.gid == gid)
+                return obj[prop]
+        }
     }
+
+    loadTilesets() {
+        let json = $.ajax({
+            url: "..\/game\/fp-env.json",
+            dataType: 'json',
+            async: false,
+        }).responseJSON;
+        let tilesets = json['tilesets']
+        this.tilesets = tilesets.map(item => {
+            return {
+                "image": '..\/game\/' + item.image,
+                "gid": item.firstgid,
+                "name": item.name,
+            }
+        });
+    }
+
+    preload() {
+
+        this.loadTilesets();
+
+
+        this.load.tilemapTiledJSON('env', '..\/game\/fp-env.json');
+
+        for (let i = 0; i < this.tilesets.length; i++) {
+            let obj = this.tilesets[i]
+            this.load.image(obj.name, obj.image)
+        }
+
+    }
+
+    create() {
+
+        const map = this.make.tilemap({ key: 'env' });
+
+        const groundLayer = map.addTilesetImage('Ground_02')
+        const bgLayer = map.addTilesetImage('background')
+
+        map.createLayer('Tile Layer 1', [groundLayer, bgLayer], 0, 0).setScale(1.1);
+
+
+        let objLayer = map.getObjectLayer('Object Layer 1')['objects'];
+
+        const objs = this.physics.add.staticGroup()
+        objLayer.forEach(object => {
+            let obj = objs.create(object.x, object.y, this.getObjPropertyFromGid(object.gid, 'name'));
+            if (this.getObjPropertyFromGid(object.gid, 'name') == 'Grass_01')
+                obj.setOrigin(0)
+
+        });
+    }
+
+
 }
