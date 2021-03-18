4f5ded5a280fed237c779ba85e6c689c5f60909b
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-03-18 15:20:09 +0100

    fix GameOver msg alignment

--- public/js/GameOverScene.js
+++ public/js/GameOverScene.js
@@ -7,7 +7,8 @@ class GameOverScene extends Phaser.Scene {
 
   create() {
     this.add.image(0, 0, "go_background").setOrigin(0).setDepth(0);
-    let label = this.add.text(gameWidth/3.5, gameHeight/2.5, "Game Over!", { fontSize: '50px', fontFamily: 'PS2P', align: 'center', fill: '#fff' })
+    let label = this.add.text(gameWidth / 2, gameHeight / 2, "Game Over!", { fontSize: '50px', fontFamily: 'PS2P', align: 'center', fill: '#fff' })
+    label.setOrigin(0.5, 0.5)
     setTimeout( () => {
         this.scene.start("MenuScene");
     }, 4000) 
