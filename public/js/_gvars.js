c7e44ccec766b2e4e99dd88b8d5c2d405a6f7205
Hakeem Beldjoudi <ih_beldjoudi@esi.dz>
2021-04-19 03:59:44 +0100

    format files | tabulation

--- public/js/_gvars.js
+++ public/js/_gvars.js
@@ -1,6 +1,8 @@
-const Label0Css = (fSize, fill, align = "center") => { return {
-    fontSize: fSize,
-    fontFamily: "PS2P",
-    align: align,
-    fill: fill,
-  }}
+const Label0Css = (fSize, fill, align = "center") => {
+    return {
+        fontSize: fSize,
+        fontFamily: "PS2P",
+        align: align,
+        fill: fill,
+    }
+}
